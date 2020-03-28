d65e1c5ec6c78abb243faf23d5f0f39c1fccefec
bulatko <buloch03@yandex.ru>
2020-03-29 02:04:04 +0300

    added bd.php

---
+++ bd.php
@@ -0,0 +1,23 @@
+<?php
+include 'CONSTS.php';
+set_time_limit(3000);
+ini_set('display_errors', 1);
+header("Content-Type: text/html; charset=utf-8");
+mb_internal_encoding("UTF-8");
+/* Переменные для соединения с базой данных */
+$hostname = "localhost";
+$username = $USERNAME;
+$password = $PASS;
+$dbName = $DBNAME;
+$mysqli = new mysqli($hostname, $username, $password, $dbName);
+mysqli_set_charset($mysqli, 'utf8mb4');
+/* проверяем соединение */
+if (mysqli_connect_errno()) {
+    printf("Ошибка соединения: %s\n", mysqli_connect_error());
+    exit();
+}
+$mysqli->query('SET NAMES utf8mb4_unicode_ci.');
+$mysqli->query("SET CHARACTER SET 'utf8mb4_unicode_ci.'");
+
+
+?>
